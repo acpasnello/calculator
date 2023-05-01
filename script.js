ddb29c1283ef6dfe77d9db23f58503386c422b8f
acpasnello <anthony.pasnello@gmail.com>
2023-05-01 18:25:24 -0400

    Functions for operations and checking inputs

--- script.js
+++ script.js
@@ -2,23 +2,116 @@ let firstNum;
 let secondNum;
 let operator;
 
-function operate(firstNum, secondNum, operator) {
-    let result = (function (){
-        switch (operator) {
-            case "+":
-                return add(firstNum, secondNum);
-            case "-":
-                return subtract(firstNum, secondNum);
-            case "*":
-                return multiply(firstNum, secondNum);
-            case "/":
-                return divide(firstNum, secondNum);
-            default:
-                break;
+let equationDisplay = document.querySelector('#equation')
+let submit = document.querySelector('#equals')
+let clear = document.querySelector('#clear')
+let resultDisplay = document.querySelector('#result')
+
+function operate() {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(Number(firstNum), Number(secondNum));
+            break;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            break;
+        case "*":
+            result = multiply(firstNum, secondNum);
+            break;
+        case "÷":
+            result = divide(firstNum, secondNum);
+            break;
+        default:
+            break;
+    }
+    if (result % 1 != 0) {
+        let temp = result - Math.floor(result)
+        temp = temp.toString();
+        if (temp.length > 9) {
+            result = result.toFixed(9)
         }
-    });
+    }
+    constructEquation();
+    resultDisplay.textContent = result;
+    return result;
 }
 function add(x, y) {return x + y;}
 function subtract(x, y) {return x - y;}
 function multiply(x, y) {return x * y;}
 function divide(x, y) {return x / y;}
+
+// Function to update display
+function constructEquation() {
+    resultDisplay.textContent = "";
+    let firstDisplay = (firstNum == null) ? "" : firstNum;
+    let operatorDisplay = (operator == null) ? "" : operator;
+    let secondDisplay = (secondNum == null) ? "" : secondNum;
+    equationDisplay.textContent = `${firstDisplay} ${operatorDisplay} ${secondDisplay}`
+}
+
+function processDigit(input) {
+    if (!firstNum) {
+        firstNum = input;
+        constructEquation();
+    } else if (firstNum && !operator) {
+        firstNum += input;
+        constructEquation();
+    } else if (firstNum && operator) {
+        if (!secondNum) {
+            secondNum = input;
+        } else {
+            secondNum += input;
+        }
+        constructEquation();
+    }
+}
+
+function processOperator(input) {
+    if (secondNum) {
+        let temp = input;
+        let result = operate();
+        // Send result and new operator to display function
+        firstNum = result;
+        secondNum = null;
+        operator = temp;
+        constructEquation();
+    } else if (!firstNum && !secondNum) {
+        return false;
+    } else if (!operator) {
+        operator = input;
+        constructEquation();
+    } else if (operator && !secondNum) {
+        operator = input;
+        constructEquation();
+    } 
+}
+// Operands
+let operands = document.querySelectorAll('.operand')
+operands.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+        processDigit(event.currentTarget.textContent);
+    })
+})
+
+// Operators
+let operators = document.querySelectorAll('.operator')
+operators.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+        processOperator(event.currentTarget.textContent)
+    })
+})
+
+// Clear Button
+clear.addEventListener('click', function() {
+    equationDisplay.textContent = "";
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    constructEquation();
+})
+
+// Equals
+submit.addEventListener('click', function() {
+    if (firstNum && operator && secondNum) {operate();}
+})
