e0fa0c109323010f5e30194c1f870f1805096c5e
acpasnello <anthony.pasnello@gmail.com>
2023-05-03 14:03:12 -0400

    Simplify keydown event processing

--- keyboardInput.js
+++ keyboardInput.js
@@ -1,17 +1,15 @@
 document.addEventListener('keydown', (event) => {
     if (event.key >= 0 && event.key <= 9) {
         processDigit(event.key)
-    } else if (event.key == "-") {
-        processOperator(event.key)
-    } else if (event.key == "+") {
-        processOperator(event.key)
-    } else if (event.key == "*") {
+    } else if (event.key == "-" || event.key == "+" || event.key == "*") {
         processOperator(event.key)
     } else if (event.key == "/") {
+        event.preventDefault();
         processOperator("÷")
     } else if (event.key == "Enter") {
         processEquals();
     } else if (event.key == "Backspace") {
+        event.preventDefault();
         processBackspace();
     }
 })
